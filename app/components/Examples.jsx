52ca1e075cae3ca2cd4e89d0e504802d6ea1bdde
ekunce <eduard.kunce@gmail.com>
2017-04-21 22:39:35 +0200

    Refactoring to stateless functional components

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,11 +1,9 @@
 var React = require('react')
 
-var Examples = React.createClass({
-    render () {
-        return (
-            <h3>Examples component</h3>
-        )
-    }
-})
+var Examples = (props) => {
+    return (
+        <h3>Examples component</h3>
+    )
+}
 
 module.exports = Examples

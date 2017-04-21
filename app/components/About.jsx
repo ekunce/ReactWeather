ed062f757a4d25328c601e8df6dc11d7602edd76
ekunce <eduard.kunce@gmail.com>
2017-04-22 00:29:49 +0200

    Styling About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,19 @@ var React = require('react')
 var About = (props) => {
     return (
         <div>
-            <h3>About</h3>
-            <p>Welcome to about page</p>
+            <h1 className="text-center">About</h1>
+            <p>This is a weather application built on React. I have built this for learning purposes</p>
+            <p>
+                Here are some of the tools used:
+            </p>
+            <ul>
+                <li>
+                    <a href="https://facebook.github.io/react">React</a> - JS framework used
+                </li>
+                <li>
+                    <a href="http://openweathermap.org">Open Weather Map</a> - Used for weather data
+                </li>
+            </ul>
         </div>
     )
 }

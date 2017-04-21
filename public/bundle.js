1ac64e565bcb828dd3fcc321f6c59839c002b4c5
Eduard Kunce <eduard.kunce@gmail.com>
2017-04-21 16:09:04 +0200

    Loading message

--- public/bundle.js
+++ public/bundle.js
@@ -13299,26 +13299,47 @@ var Weather = React.createClass({
     displayName: 'Weather',
     getInitialState: function getInitialState() {
         return {
-            location: 'Miami',
-            temp: 88
+            isLoading: false
         };
     },
     handleSearch: function handleSearch(location) {
         var that = this;
+
+        this.setState({
+            isLoading: true
+        });
+
         openWeatherMap.getTemp(location).then(function (temp) {
             that.setState({
                 location: location,
-                temp: temp
+                temp: temp,
+                isLoading: false
             });
         }, function (errorMessage) {
+            that.setState({
+                isLoading: false
+            });
             alert(errorMessage);
         });
     },
     render: function render() {
         var _state = this.state,
+            isLoading = _state.isLoading,
             location = _state.location,
             temp = _state.temp;
 
+
+        function renderMessage() {
+            if (isLoading) {
+                return React.createElement(
+                    'h3',
+                    null,
+                    'Fetching weather ... '
+                );
+            } else if (temp && location) {
+                return React.createElement(WeatherMessage, { location: location, temp: temp });
+            }
+        }
 
         return React.createElement(
             'div',
@@ -13329,7 +13350,7 @@ var Weather = React.createClass({
                 'Weather component'
             ),
             React.createElement(WeatherForm, { onSearch: this.handleSearch }),
-            React.createElement(WeatherMessage, { location: location, temp: temp })
+            renderMessage()
         );
     }
 });

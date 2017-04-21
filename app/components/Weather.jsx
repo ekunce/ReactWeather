3f3bf9378df14f30a361da398e45f5e97401c630
Eduard Kunce <eduard.kunce@gmail.com>
2017-04-21 13:40:10 +0200

    initial commit

---
+++ app/components/Weather.jsx
@@ -0,0 +1,11 @@
+var React = require('react')
+
+var Weather = React.createClass({
+    render () {
+        return (
+            <h3>Weather component</h3>
+        )
+    }
+})
+
+module.exports = Weather
